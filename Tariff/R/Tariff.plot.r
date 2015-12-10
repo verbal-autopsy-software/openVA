b4a96ee1e834b1676c99ee4e57f9e56a92c57168
richardli <lizehang@gmail.com>
2015-12-09 17:44:10 -0800

    add pre-beta Tariff

---
+++ Tariff/R/Tariff.plot.r
@@ -0,0 +1,14 @@
+#' Plot CSMF of the results obtained from Tariff algorithm
+#'
+#' This function plots the CSMF of the fitted results.
+#'
+#' @param tariff fitted object from \link{Tariff}
+#' @param csmf.train option to either combine CSMF from training data (set to "combine"), or to compare with training CSMF (set to "compare"), or to ignore it (set to "none")
+#' @param ... Arguments to be passed to/from graphic function
+#'
+#'
+
+Tariff.csmf <- function(tariff, csmf.train = c("combine", "compare", "none")[3], ...){
+	
+
+}
---
+++ Tariff/R/Tariff_1.0-package.R
@@ -0,0 +1,40 @@
+
+
+#' Translation list of COD codes
+#' 
+#' This is the translation of COD abbreviation codes into their corresponding
+#' full names.
+#' 
+#' 
+#' @name causetext
+#' @docType data
+#' @format A data frame with the translation of COD codes to their names on 68
+#' CODs (both the version of COD only and COD with group code).
+#' @keywords datasets
+#' @examples
+#' 
+#' data(causetext)
+#' 
+NULL
+
+
+
+#' 200 records of Sample Input
+#' 
+#' This is a dataset consisting of 200 arbitrary sample input deaths in the
+#' format of InterVA4 symptom input, with additional column at the end indicating
+#' their cause. The causes are simulated and does not represent true causes. 
+#' 
+#' 
+#' @name TariffInput
+#' @docType data
+#' @format 200 arbitrary input records.
+#' @keywords datasets
+#' @examples
+#' 
+#' data(TariffInput)
+#' 
+NULL
+
+
+
