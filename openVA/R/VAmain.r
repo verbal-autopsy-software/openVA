3ab5ddb0b6810e501a65626c8f9ee8ea7b5c3dc7
Richard Li <lizehang@gmail.com>
2022-02-12 20:09:06 -0800

    typo fix

--- openVA/R/VAmain.r
+++ openVA/R/VAmain.r
@@ -109,7 +109,7 @@ codeVA <- function(data, data.type = c("WHO2012", "WHO2016", "PHMRC", "customize
     if(is.null(data.train) || is.null(causes.train)){
       stop("Error: need training data for customized input.")
     }
-    tmp <- data.test[, -1]
+    tmp <- data[, -1]
     tmp <- tmp[, colnames(tmp)!=causes.train]
     tmp <- toupper(as.character(as.matrix(tmp)))
     if(sum(!tmp %in% c("Y", "", "N", ".", "-")) != 0){
